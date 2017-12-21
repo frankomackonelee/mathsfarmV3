778c62b7a2f31f3206d2ce8270b405423ada7ff2
frankomackonelee <fmalone-lee@hotmail.co.uk>
2017-12-21 14:27:01 +0000

    small tweeks

--- js/question_selector.js
+++ js/question_selector.js
@@ -1,9 +1,28 @@
+
+
 (function () {
 'use strict';
 
 angular.module('quesion_selector_app', [])
 
-.controller('QSController', function($scope){
+.service('sharedProperties', function () {
+  var property = [];
+    return {
+        getProperty: function () {
+            return property;
+        },
+        setProperty: function(value) {
+            property = value;
+        }
+    };
+})
+
+.controller('QSController', QSController)
+
+.controller('PageController', PageController);
+
+QSController.$inject = ['$scope', 'sharedProperties']
+function QSController($scope, sharedProperties){
   var topics = [];
   for(var i = 0; i<topics_and_subtopics.data.length;i++){
     topics.push(topics_and_subtopics.data[i].parent);
@@ -19,29 +38,57 @@ angular.module('quesion_selector_app', [])
           return unfilteredObj.data[i].children;
         }
     }
-  }
+  };
 
   $scope.topicChange = function(){
     console.log("Changed topic");
     $scope.subtopics = returnRelevantArray(topics_and_subtopics,$scope.selectedTopic);
     $scope.selectedSubtopic = "NotSelected"
     console.log("Subtopics have been changed");
-  }
+  };
 
   $scope.subtopicChange = function(){
     console.log("Changing subtopic");
     $scope.titles = returnRelevantArray(subtopics_and_titles,$scope.selectedSubtopic);
     $scope.selectedTitle = "NotSelected"
     console.log("Titles have been changed");
-  }
+  };
 
   $scope.titleChange = function(){
     console.log("Changing titles");
-    $scope.qandas = returnRelevantArray(titles_and_questions,$scope.selectedTitle);
+    sharedProperties.setProperty( returnRelevantArray(titles_and_questions,$scope.selectedTitle) );
     console.log("Questions Have Changed");
+  };
+
+}
+
+PageController.$inject['$scope','sharedProperties']
+function PageController($scope, sharedProperties){
+  $scope.visibility = [];
+
+  var fillVisibility = function(fillTo){
+    for(var i = $scope.visibility.length; i<fillTo; i++){
+      $scope.visibility.push(false);
+    }
+  };
+
+  $scope.clickResponse = function(int){
+    if(int > $scope.length){
+      fillVisibility(int);
+    }
+    $scope.visibility[int] = !$scope.visibility[int];
+  }
+  $scope.visible = function(int){
+    return $scope.visibility[int];
+  }
+  $scope.invisible = function(int){
+    return !$scope.visible(int);
   }
 
-});
+  $scope.qandas = function(){
+    return sharedProperties.getProperty();
+  }
 
+}
 
 })();

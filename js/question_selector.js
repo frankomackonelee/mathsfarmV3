6e653f038af1ed0defbd573aeb1bd5a9021f3922
frankomackonelee <fmalone-lee@hotmail.co.uk>
2017-12-22 21:56:27 +0000

    pulling out templates from html

--- js/question_selector.js
+++ js/question_selector.js
@@ -1,115 +1,136 @@
-
-
 (function () {
-'use strict';
 
-angular.module('quesion_selector_app', [])
+  'use strict';
 
-.service('sharedProperties', sharedProperties)
+  var thisApp = angular.module('quesion_selector_app', [])
+  thisApp.service('sharedProperties', sharedProperties)
+  thisApp.controller('QSController', QSController)
+  thisApp.controller('PageController', PageController)
+  thisApp.directive('topMenuBar', TopMenuBar);
+  thisApp.directive('questionSelection',QuestionSelection)
 
-.controller('QSController', QSController)
-
-.controller('PageController', PageController);
-
-QSController.$inject = ['sharedProperties']
-function QSController(sharedProperties){
-  var qs = this;
-
-  var topics = [];
-  for(var i = 0; i<topics_and_subtopics.data.length;i++){
-    topics.push(topics_and_subtopics.data[i].parent);
+  /*Provides the html for the top menu bar
+  See lecture 26
+  */
+  function TopMenuBar(){
+    var ddo = {
+      templateUrl: './html/top_menu_bar.html'
+    }
+    return ddo;
   }
-  qs.topics = topics;
-  qs.selectedTopic = "NotSelected";
-  qs.selectedSubtopic = "NotSelected";
-  qs.selectedTitle = "NotSelected";
 
-  var returnRelevantArray = function(unfilteredObj, selectedValue){
-    for(var i = 0; i<unfilteredObj.data.length; i++){
-        if(unfilteredObj.data[i].parent == selectedValue){
-          return unfilteredObj.data[i].children;
+  /*Provides the html for the dropdown selectors
+  */
+  function QuestionSelection(){
+    var ddo = {
+      templateUrl: './html/question_selection.html',
+    }
+    return ddo;
+  }
+
+  /*Controller responsible for the question sellectors
+  which are common to all resource pages
+  */
+  QSController.$inject = ['sharedProperties']
+  function QSController(sharedProperties){
+    var qs = this;
+
+    var topics = [];
+    for(var i = 0; i<topics_and_subtopics.data.length;i++){
+      topics.push(topics_and_subtopics.data[i].parent);
+    }
+    qs.topics = topics;
+    qs.selectedTopic = "NotSelected";
+    qs.selectedSubtopic = "NotSelected";
+    qs.selectedTitle = "NotSelected";
+
+    var returnRelevantArray = function(unfilteredObj, selectedValue){
+      for(var i = 0; i<unfilteredObj.data.length; i++){
+          if(unfilteredObj.data[i].parent == selectedValue){
+            return unfilteredObj.data[i].children;
+          }
+      }
+    };
+
+    qs.topicChange = function(){
+      qs.subtopics = returnRelevantArray(topics_and_subtopics,qs.selectedTopic);
+      qs.selectedSubtopic = "NotSelected"
+    };
+
+    qs.subtopicChange = function(){
+      qs.titles = returnRelevantArray(subtopics_and_titles,qs.selectedSubtopic);
+      qs.selectedTitle = "NotSelected"
+    };
+
+    qs.titleChange = function(){
+      sharedProperties.setqAndAArray( returnRelevantArray(titles_and_questions,qs.selectedTitle) );
+    };
+
+  }
+
+  /*Service responsible for sharing data between
+  the controller of the question selector and
+  the controller of the page selector.
+  Only whether answers are visible need to be shared
+  */
+  function sharedProperties() {
+    var service = this;
+    var qAndAArray = [];
+    var visibilityArray = [];
+
+    service.getqAndAArray = function () {
+        return qAndAArray;
+    }
+
+    service.setqAndAArray = function(value) {
+        qAndAArray = value;
+        for(var i = 0; i<qAndAArray.length; i++){
+          visibilityArray[i] = false;
         }
     }
-  };
 
-  qs.topicChange = function(){
-    console.log("Changed topic");
-    qs.subtopics = returnRelevantArray(topics_and_subtopics,qs.selectedTopic);
-    qs.selectedSubtopic = "NotSelected"
-    console.log("Subtopics have been changed");
-  };
+    service.isVisible = function(int){
+      return visibilityArray[int];
+    }
 
-  qs.subtopicChange = function(){
-    console.log("Changing subtopic");
-    qs.titles = returnRelevantArray(subtopics_and_titles,qs.selectedSubtopic);
-    qs.selectedTitle = "NotSelected"
-    console.log("Titles have been changed");
-  };
-
-  qs.titleChange = function(){
-    console.log("Changing titles");
-    sharedProperties.setqAndAArray( returnRelevantArray(titles_and_questions,qs.selectedTitle) );
-    console.log("Questions Have Changed");
-  };
-
-}
-
-PageController.$inject['sharedProperties'];
-function PageController(sharedProperties){
-  var pc = this;
-
-  pc.clickResponse = function(int){
-    sharedProperties.changeVisibiliity(int);
-  }
-
-  pc.visible = function(int){
-    return sharedProperties.isVisible(int);
-  }
-
-  pc.qandas = sharedProperties.getqAndAArray;
-
-  pc.mouseoverQuestion = function(int){
-    if(sharedProperties.isVisible(int)){
-      pc.EmphasiseClass = int;
+    service.changeVisibiliity = function(int){
+      visibilityArray[int] = !visibilityArray[int];
     }
   }
 
-  pc.mouseoutQuestion = function(){
-    pc.EmphasiseClass = null;
-  }
+  /*Responsible for control of the content of a particular page
+  */
+  PageController.$inject['sharedProperties'];
+  function PageController(sharedProperties){
+    var pc = this;
 
-  pc.GetEmphasis = function(int){
-    if(int == pc.EmphasiseClass){
-      return "bg-info";
-    }else{
-      return "";
+    pc.clickResponse = function(int){
+      sharedProperties.changeVisibiliity(int);
     }
-  }
-}
 
-function sharedProperties() {
-  var service = this;
-  var qAndAArray = [];
-  var visibilityArray = [];
+    pc.visible = function(int){
+      return sharedProperties.isVisible(int);
+    }
 
-  service.getqAndAArray = function () {
-      return qAndAArray;
-  }
+    pc.qandas = sharedProperties.getqAndAArray;
 
-  service.setqAndAArray = function(value) {
-      qAndAArray = value;
-      for(var i = 0; i<qAndAArray.length; i++){
-        visibilityArray[i] = false;
+    pc.mouseoverQuestion = function(int){
+      if(sharedProperties.isVisible(int)){
+        pc.EmphasiseClass = int;
       }
-  }
+    }
 
-  service.isVisible = function(int){
-    return visibilityArray[int];
-  }
+    pc.mouseoutQuestion = function(){
+      pc.EmphasiseClass = null;
+    }
 
-  service.changeVisibiliity = function(int){
-    visibilityArray[int] = !visibilityArray[int];
+    pc.GetEmphasis = function(int){
+      if(int == pc.EmphasiseClass){
+        return "bg-info";
+      }else{
+        return "";
+      }
+    }
   }
-}
 
 })();
